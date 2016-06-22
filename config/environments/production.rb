ff1b723993e8168eb7e41bab3065dfde32fab3b4
Nevan Whiteside <nevanwhiteside@gmail.com>
2016-06-22 19:08:43 -0400

    s3 region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,6 @@ Rails.application.configure do
   #S3 Uploads
   config.paperclip_defaults = {
   storage: :s3,
-  :url => "s3-us-west-2.amazonaws.com",
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
